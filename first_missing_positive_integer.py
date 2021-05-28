0e01218b03e26f1295a3bc3386406b65800af1bf
vzbrunstad <vzbrunstad@msn.com>
2021-05-27 20:03:25 -0500

    Vince B debugging one Py Complete

--- first_missing_positive_integer.py
+++ first_missing_positive_integer.py
@@ -1,13 +1,29 @@
-def first_missing_positive(nums)
+from typing import FrozenSet
+
+
+def first_missing_positive(nums):
+  # if empty list, return 1
   if not nums:
     return 1
+
+  # run through list to move valid values "v" to be located at index "v - 1"
+  # relocate the value "v" (0 < v < length) from the current index to index "v - 1"
+  # values outside of "0 < v < length" don't matter
+  # swap values in the list so that index "v - 1" now holds current value "v"
+  # the current index location will now hold the value that was swapped against
+  # break out of while loop if current value and swapped value are the same
   for i, num in enumerate(nums):
-  while i + 1 != nums[i] and 0 < nums[i] <= len(nums):
-      v = nums[i]+1:
-        nums[i], nums[v - 1] = nums[v - 1], nums[i]
-    if nums[i] == nums[v + 1]:
-      break
-  for i, num in enumerate(nums, 1):
-      if num != i:
+
+
+    while i + 1 != nums[i] and 0 < nums[i] <= len(nums):
+      v = nums[i]
+      nums[i], nums[v - 1] = nums[v - 1], nums[i]
+
+      if nums[i] == nums[v - 1]:
+        break #break out of while loop if current value and swapped value are the same
+
+  for i, num in enumerate(nums,1):
+    if num != i:
       return i
+
   return len(nums) + 1
---
+++ first_missing_positive_integer_Spec.py
@@ -0,0 +1,6 @@
+from first_missing_positive_integer import first_missing_positive
+
+
+print ( first_missing_positive([3, 4, -1, 1]) == 2)
+print ( first_missing_positive([3, 2, -1, 1]) == 4)
+print(first_missing_positive([1, 2, 3]) == 4)
